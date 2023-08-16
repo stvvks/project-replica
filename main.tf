a4aecf70b6b7ba50fd7b1e63efa7d101e71fcad9
Jeremiah Nkumsah <99621190+stvvks@users.noreply.github.com>
2023-08-16 11:34:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,10 +43,12 @@
     subnet_id = aws_subnet.privatesubnets.id
     route_table_id = aws_route_table.PrivateRT.id
  }
- resource "aws_eip" "nateIP" {
-   vpc   = true
- }
- # Creating the NAT Gateway using subnet_id and allocation_id
+
+# resource "aws_eip" "nateIP" {
+# vpc   = true
+#}
+
+# Creating the NAT Gateway using subnet_id and allocation_id
  resource "aws_nat_gateway" "NATgw" {
    allocation_id = aws_eip.nateIP.id
    subnet_id = aws_subnet.publicsubnets.id
